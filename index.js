3df49c7184621b5093857010259fb3613bb2ccb4
Rafał Rybacki <rafal@onegini.com>
2021-11-19 17:28:49 +0100

    Use input

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ async function run() {
       method: 'get',
       url: `https://onewelcome.atlassian.net/rest/api/3/issue/${issuekey}?fields=issuetype`,
       auth: {
-        username: process.env.JIRA_API_USERNAME,
-        password: process.env.JIRA_API_TOKEN
+        username: core.getInput('JIRA_API_USERNAME'),
+        password: core.getInput('JIRA_API_TOKEN')
       },
       headers: {
         'Content-Type': 'application/json'
